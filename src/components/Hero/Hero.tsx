dc69c1cb7e6a743e092a79f282e2eee38de97a58
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-16 16:36:52 +0100

    style: styled the hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,13 @@
+import Logo from "../Logo/Logo";
+
 function HeroSection() {
   return (
-    <div className="mt-16">
-      <h1 className="text-7xl text-center">Zubi</h1>
-      <p className="text-center mt-4">Bridge your skills</p>
+    <div className="h-screen mt-16 bg-zubiGreen flex flexcol justify-center content-center font-helonik text-white">
+      <div className="hero-content flex flex-col items-center gap-6 mt-44">
+        <Logo className="w-32" />
+        <h1 className="text-8xl text-center">zubi</h1>
+        <p className="text-center mt-4 text-2xl">Bridge your skills</p>
+      </div>
     </div>
   );
 }
