537bd1cc5a7e2637addf7feda19b65262f0099b4
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-16 17:10:55 +0100

    feat: add about and tutorprofile pages with routing

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar/Navbar";
+import HeroSection from "../components/Hero/Hero";
+import CardsContainer from "../components/CardsContainer/CardsContainer";
+import Footer from "../components/Footer/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <CardsContainer />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/TutorProfile.tsx
@@ -0,0 +1,5 @@
+const TutorProfile = () => {
+  return <h1>Tutor Profile</h1>;
+};
+
+export default TutorProfile;
