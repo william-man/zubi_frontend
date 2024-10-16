9e76a34de36f087c7bb80828740ff4d73603ef20
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-16 17:44:59 +0100

    style: change logo size in hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ function HeroSection() {
   return (
     <div className="h-screen mt-16 bg-zubiGreen flex flexcol justify-center content-center font-helonik text-white">
       <div className="hero-content flex flex-col items-center mt-44">
-        <Logo className="w-32" />
+        <Logo className="w-28" />
         <h1 className="text-8xl text-center">zubi</h1>
         <p className="text-center mt-4 mb-16 text-2xl">Bridge your skills</p>
         <Button
