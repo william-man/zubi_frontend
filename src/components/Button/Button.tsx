9199eff19a9e59f6ada4b2c0bd9f713ea5a65417
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-16 17:42:05 +0100

    style: increase size of tutor card button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -12,7 +12,7 @@ const Button: React.FC<ButtonProps> = ({ label, onClick, buttonType }) => {
       onClick={onClick}
       className={
         buttonType === "cardButton"
-          ? "bg-buttonBg text-zubiText font-helonik py-2 px-4 rounded-full transition duration-300 ease-in-out hover:bg-zubiLogo"
+          ? "bg-buttonBg text-zubiText font-helonik py-3 px-11  text-lg rounded-full transition duration-300 ease-in-out hover:bg-zubiLogo"
           : "bg-buttonBg text-zubiText font-helonik py-4 px-6 text-xl rounded-full transition duration-300 ease-in-out hover:bg-zubiLogo"
       }
       style={{ margin: "2px" }}
