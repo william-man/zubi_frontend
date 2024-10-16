0f74118c2f0d33b128905765a9f8255b557ebc40
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-16 21:38:58 +0100

    feat: add calendar to tutor profile

--- src/pages/TutorProfile.tsx
+++ src/pages/TutorProfile.tsx
@@ -1,13 +1,17 @@
 import { useLocation } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
+import { DayPicker } from "react-day-picker";
+import { useState } from "react";
+import "react-day-picker/style.css";
 
 const TutorProfile = () => {
   const location = useLocation();
   const { tutor } = location.state;
+  const [selected, setSelected] = useState<Date>();
 
   return (
     <Layout>
-      <div className="h-screen bg-zubiGreen font-helonik flex flex-col items-center text-white">
+      <div className="min-h-screen bg-zubiGreen font-helonik flex flex-col items-center text-white">
         <div className="w-10/12 mx-auto flex flex-col items-center mt-20">
           <h3 className="text-4xl mb-5">{tutor.full_name}</h3>
           <img
@@ -15,15 +19,20 @@ const TutorProfile = () => {
             className="w-11/12 mb-8 bg-white rounded-md"
           />
           <p className="font-sans leading-5">{tutor.description}</p>
-          <div className="">
-            <h4 className="text-3xl mt-10 mb-4 self-end ">Availability</h4>
-            <div className="font-sans">
-              <p>Monday:</p>
-              <p>Tuesday:</p>
-              <p>Wednesday:</p>
-              <p>Thursday:</p>
-              <p>Friday:</p>
-            </div>
+
+          <h4 className="text-3xl mt-10 mb-4 self-end ">Availability</h4>
+          <div className="mb-12">
+            <DayPicker
+              className="font-sans"
+              mode="single"
+              selected={selected}
+              onSelect={setSelected}
+              footer={
+                selected
+                  ? `Selected: ${selected.toLocaleDateString()}`
+                  : "Pick a day."
+              }
+            />
           </div>
         </div>
       </div>
