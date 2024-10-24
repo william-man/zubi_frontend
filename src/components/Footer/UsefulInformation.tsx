191ac37ba84d97487785c05c29fe857fb8ab601a
Khalos Moscato <khalos.moscato@icloud.com>
2024-10-24 12:11:46 +0100

    feat: created careers page

--- src/components/Footer/UsefulInformation.tsx
+++ src/components/Footer/UsefulInformation.tsx
@@ -3,7 +3,7 @@ import FooterSection from './FooterSection';
 const UsefulInformation = () => {
   const usefulInfoLinks = [
     { name: 'Contact Us', url: '#' },
-    { name: 'Careers', url: '#' },
+    { name: 'Careers', url: '/careers' },
     { name: 'Donate', url: '#' },
     { name: 'FAQ', url: '#' },
     { name: 'Partners', url: '#' },
