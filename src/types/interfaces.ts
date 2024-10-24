02ecff859ea1efe86b6742974362c5b8fa7487f8
Khalos Moscato <khalos.moscato@icloud.com>
2024-10-24 10:01:16 +0100

    fix: changed way to populat tutor page by using the card container component

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -66,14 +66,3 @@ export interface UserContextType {
   user: User | null;
   setUser: (user: User | null) => void;
 }
-
-export interface Tutor {
-  id: number;
-  name: string;
-  description: string;
-  [x: string]: ReactNode;
-}
-
-export interface TutorResponse extends BaseResponse {
-  data: Tutor[];
-}
