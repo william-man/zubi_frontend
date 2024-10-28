cdd2803f7bb7e0f7350ac3550f6469b284af04be
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-28 15:08:24 +0000

    refactor: change fetch requests to environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000", // Your backend server
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Strip '/api' from the request URL
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:3000", // Your backend server
+  //       changeOrigin: true,
+  //       secure: false,
+  //       rewrite: (path) => path.replace(/^\/api/, ""), // Strip '/api' from the request URL
+  //     },
+  //   },
+  // },
 });
